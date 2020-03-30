99da540da319da9c75cd4ee583c944a55ee9ddb2
Sagar Chikhale <57104076+sagar2702@users.noreply.github.com>
2020-03-30 07:57:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,14 +22,16 @@ if __name__ == "__main__":
         #print(len(infoList))
         x=len(infoList)
         #storing last row
-        last_row=infoList[x-5:x-1]
+        #last_row=infoList[x-5:x-1]
+        last_row=infoList[x-4:x-1]
         #print(last_row)
         last_row_col_1=last_row[0].split('\n')[2]
-        last_row_col_2=last_row[1]
+        last_row_col_2=last_row[1].split('\n')[1]
         last_row_col_3 = last_row[2].split('\n')[1]
-        last_row_col_4 = last_row[3].split('\n')[1]
+        #last_row_col_4 = last_row[3].split('\n')[1]
         #print(last_row_col_1,last_row_col_2, last_row_col_3, last_row_col_4)
-        detail="Indian confirmed cases  : {}\nForeigner confirmed cases: {}\nCured : {}\nDeath : {}".format(last_row_col_1,last_row_col_2,last_row_col_3,last_row_col_4)
+        #detail="Indian confirmed cases  : {}\nForeigner confirmed cases: {}\nCured : {}\nDeath : {}".format(last_row_col_1,last_row_col_2,last_row_col_3,last_row_col_4)
+        detail="Total confirmed cases  : {}\nCured : {}\nDeath : {}".format(last_row_col_1,last_row_col_2,last_row_col_3)
 
         notifyMe("CORONA'S EFFECT SO FAR IN INDIA", detail)
         time.sleep(30)
