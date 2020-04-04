5d9fe47a060b27a467987f38aa5d4b2509d3a211
Sagar Chikhale <57104076+sagar2702@users.noreply.github.com>
2020-04-04 18:53:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,15 +18,14 @@ if __name__ == "__main__":
             str+=x.text
         #print(str)
         infoList=str.split('\n\n')
-        #print(infoList)
+        # print(infoList)
         #print(len(infoList))
         x=len(infoList)
         #storing last row
-        #last_row=infoList[x-5:x-1]
-        last_row=infoList[x-4:x-1]
+        last_row=infoList[x-5:]
         #print(last_row)
         last_row_col_1=last_row[0].split('\n')[2]
-        last_row_col_2=last_row[1].split('\n')[1]
+        last_row_col_2=last_row[1]
         last_row_col_3 = last_row[2].split('\n')[1]
         #last_row_col_4 = last_row[3].split('\n')[1]
         #print(last_row_col_1,last_row_col_2, last_row_col_3, last_row_col_4)
